270b72663aa1d41867c251955b05266df3500dd3
Guilherme Guimaraes <gillguimaraes@Guilhermes-MacBook-Pro-2.local>
2023-11-30 15:22:05 -0500

    button styling added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// write your JavaScript here
+let priceBtn = document.getElementsByClassName("price-btn");
+
+    
+// priceBtn.addEventListener('mouseover', (event)=>{this.style.color = "#bc1e4a"});
+// onmouseover = (event) => {
+//     event.target.style.color = "red";
+// }
+
+// let mouseOverFunction = function(){
+//     this.style.color = 'blue';
+// }
+
+priceBtn.onmouseover = function(){
+    alert("test");
+};
